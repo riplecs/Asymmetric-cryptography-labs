cb23c07d2faf329e1ea4e0c53b3da69ba84e8db1
riplecs <48178483+riplecs@users.noreply.github.com>
2021-10-20 17:45:51 +0300

    Update laba2.py

--- laba2.py
+++ laba2.py
@@ -8,6 +8,7 @@ Created on Thu Oct  7 18:13:52 2021
 from laba1 import generate_state
 from laba1 import lfsr
 from laba1 import L20
+import requests as r
 import random
 import gmpy2
 import time
@@ -15,6 +16,7 @@ import time
 
 PRIMES = [2, 3, 5, 7, 11, 13, 17, 23, 29, 31, 37, 41, 43]
 BITS = 32
+COMPOSITE_NUMS = []
 
 
 def decomposition(n):
@@ -117,3 +119,62 @@ def ReceiveKey(d1, k1, s1, my_public_key, public_key):
     s = Decrypt(s1, d1, my_public_key)
     return k == Encrypt(s, public_key)
     
+    
+def RSA():
+    p1 = choice_big_prime(2**(BITS - 1), 2**BITS - 1, BITS)
+    q1 = choice_big_prime(2**(BITS - 1), 2**BITS - 1, BITS)
+    while q1 == p1:
+        q1 = choice_big_prime(2**(BITS - 1), 2**BITS - 1, BITS)
+    p2 = choice_big_prime(p1, 2**BITS - 1, BITS)
+    q2 = choice_big_prime(q1, 2**BITS - 1, BITS)
+    while p2 == q2:
+        q2 = choice_big_prime(2**(BITS - 1), 2**BITS - 1, BITS)
+    d1, A_public_key = GenerateKeyPair(p1, q1)
+    d2, B_public_key = GenerateKeyPair(p2, q2)
+    while B_public_key[1] <  A_public_key[1]:
+        d1, A_public_key = GenerateKeyPair(p1, q1)
+    M = conv(lfsr(generate_state(20), L20, 20, BITS))
+    while M >= B_public_key[1] or M >= A_public_key[1]:
+        M = conv(lfsr(generate_state(20), L20, 20, BITS))
+    print('Випадкове повідомлення:', M)
+    print('\nВідкритий ключ юзера А:')
+    print(f'(e1, n1) = ({A_public_key[0]}, {A_public_key[1]})')
+    print('\nВідкритий ключ юзера В:')
+    print(f'(e2, n2) = ({B_public_key[0]}, {B_public_key[1]})')
+    C = Encrypt(M, B_public_key)
+    print('\nЮзер А зашифровує повідомлення, використовуючи '
+          'відкритий ключ юзера В:', C, 'і надсилає йому.')
+    print('\nЮзер В розшифровує повідомлення, використовуючи  '
+          'cвій секретний ключ: ', Decrypt(C, d2, B_public_key))
+    print('\n***Тепер обміняємось повідомленням із сайтом***')
+    print('\nНехай сайт надсилатиме юзеру А зашифроване повідомлення.')
+    S_key = r.get('http://asymcryptwebservice.appspot.com/rsa/serverKey?'
+                  f'keySize={BITS}').json()
+    print('\nЮзер А обмінюється відкритими ключами із сайтом. '
+          'Проводиться перевірка, чи модуль юзера А менший за модуль сайту, '
+          'і, в разі необхідності, сайт змінює свій модуль.')
+    while A_public_key[1] < int(S_key['modulus'], 16):
+        S_key = r.get('http://asymcryptwebservice.appspot.com/rsa/serverKey?'
+                  f'keySize={BITS}').json()
+    print('\nВідкритий ключ cайту:')
+    print(f'(e_s, n_s) = ({int(S_key["publicExponent"], 16)},' 
+          f'{int(S_key["modulus"], 16)})')
+    E = hex(A_public_key[0])[2:]
+    N = hex(A_public_key[1])[2:]
+    M = conv(lfsr(generate_state(20), L20, 20, BITS))
+    C = r.get('http://asymcryptwebservice.appspot.com/rsa/encrypt?modulus='
+              f"{N}&publicExponent={E}"
+              f'&message={hex(M)[2:]}&type=BYTES')
+    CipherText = int(C.json()['cipherText'], 16)
+    print('\nЗашифроване повідомлення, що отримав юзер А:', CipherText)
+    print('\nЮзер А розшифровує його за допомогою свого секретного ключа. ')
+    Dec = Decrypt(CipherText, d1, A_public_key)
+    if Dec == M:
+        print('\nЮзер А успішно розшифрував повідомлення:', Dec)
+    else:
+        print('\nЩось пішло не так!')
+    
+print('\n***Приклади обміну повідомлення в схемі RSA***\n')
+RSA()
+#print('Числа, що не пройшли перевірки на простоту: ')
+#print(COMPOSITE_NUMS)
