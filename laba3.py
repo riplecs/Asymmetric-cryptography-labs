d569d4afdccbbc7b7ff789a105661206fa058a7f
riplecs <48178483+riplecs@users.noreply.github.com>
2021-12-05 14:32:01 +0200

    Update laba3.py

--- laba3.py
+++ laba3.py
@@ -30,8 +30,8 @@ def GenerateKeyPair():
     return p, q, b, n
     
 
-def ExtGCD(p, q):
-    r1, r2 = p, q
+def ExtGCD(a, b):
+    r1, r2 = a, b
     s1, s2 = 1, 0
     t1, t2 = 0, 1
     while r2 != 0:
